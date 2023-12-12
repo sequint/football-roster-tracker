7e6859202b010c360ab128a8877699e027d8ad72
stevenquintana <steve.e.quint@gmail.com>
2023-12-12 11:35:20 -0800

    Add getPlayer and addPlayer methods

--- src/FootballRoster.java
+++ src/FootballRoster.java
@@ -5,11 +5,33 @@ public class FootballRoster {
 	
 	// Default constructor
 	public FootballRoster() {
-		this.rosterArray = new Player[1000];
+		this.rosterArray = new Player[1000]; // Allocate enough memory to cover changing size of array contents
 		this.totalPlayers = 0;
 	}
 	
 	public int getTotalNumberOfPlayers() {
 		return this.totalPlayers;
+	}
+	
+	// Gets a player with an index passed
+	public Player getPlayer(int index) {
+		// If the index passed is outside the bounds of non-null elements return null
+		if (index < 0 || index >= this.totalPlayers) {
+			return null;
+		}
+		
+		return this.rosterArray[index];
+	}
+	
+	public void addPlayer(Player newItem) {
+		if (newItem == null) {
+			System.out.println("Player not added");
+		}
+		// Add the new player to the next null element in array
+		// and increment the total amount of players in the array
+		else {
+			this.rosterArray[this.totalPlayers] = newItem;
+			this.totalPlayers++;
+		}
 	}
 }
--- src/TeamRosterTrackerInterface.java
+++ src/TeamRosterTrackerInterface.java
@@ -3,6 +3,7 @@ public class TeamRosterTrackerInterface {
 
 	public static void main(String[] args) {
 		
+		// To be updated, for test only
 		Player player1 = new Player("Steven", "QB", 230.45, false);
 		
 		System.out.println(player1.getName());
@@ -15,6 +16,14 @@ public class TeamRosterTrackerInterface {
 		
 		System.out.println();
 		System.out.println(roster1.getTotalNumberOfPlayers());
+		
+		roster1.addPlayer(player1);
+		roster1.addPlayer(player1);
+		
+		System.out.println(roster1.getTotalNumberOfPlayers());
+		
+		Player testPlayer = roster1.getPlayer(1);
+		System.out.println(testPlayer.getName());
 
 	}
 
